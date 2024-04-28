9251265d5a6ea3ab7c407a4ee397a699db86c67c
shahoriarniloy <niloyshahoriar@gmail.com>
2024-04-28 21:00:27 +0600

    Art & Craft Add Operation

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors());
+app.use(express.json());
 
 
-const users =[
+const arts =[
     {
       id: 1,
       image: "https://i.ibb.co/wJKT9vL/48a8b088688ac81138c2742a3805451e.jpg",
@@ -42,15 +43,65 @@ const users =[
       processing_time: "1-2 weeks"
     },
   ];
+
+
+
+  
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://niloyshahoriar:WcBAHfnEVRLwhjm7@cluster0.dxgrzuk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("artsDB");
+    const artsCollection =database.collection("arts");
+
+    
+    app.post('/arts',async(req,res)=>{
+        const newArts= req.body;
+        newArts.id = arts.length+1;
+        // arts.push(newArts);
+        // res.send(newArts);
+        result = await artsCollection.insertOne(newArts);
+        res.send(result);
+    });
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
   
 app.get('/',(req,res)=>{
     res.send('Artstry server')
 });
 
+
 app.get('/arts',(req,res)=>{
-    res.send(users);
+    res.send(arts);
 
 })
+
+
 app.listen(port,()=>{
     console.log(`Server is running on port: ${port}`);
 })
